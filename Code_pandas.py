04808865a200d3cc7036ba8339666557aa8fc2ec
MD SAYEM HOSSEN <sayempatwary57@gmail.com>
2025-12-28 23:34:25 +0600

    Add files via upload

---
+++ Code_pandas.py
@@ -0,0 +1,61 @@
+
+import pandas as pd
+#
+# mydataset = {
+#   'cars': ["BMW", "TOYTA", "Ford"],
+#   'passings': [2, 7, 5]
+# }
+#
+# myvar = pd.DataFrame(mydataset)
+#
+# print(myvar)
+#
+# # Pandas Series
+# a = [1,2,3,4,5]
+# myvar1 = pd.Series(a)
+# print(myvar1)
+#
+# b = [6,7,8,9,10]
+# c = [11,12,1,14,15]
+#
+# myvar2 = pd.Series(b)
+#
+# print(myvar2[0])
+
+
+
+## Create Labels
+
+# import pandas as pd
+#
+# a = [3,4,5]
+#
+# mybar = pd.Series(a,index=["x","y","z"])
+# print(mybar)
+# print(mybar)
+
+
+# a = [1, 7, 2]
+#
+# myvar = pd.Series(a, index = ["x", "y", "z"])
+#
+# print(myvar["z"])
+#
+# Colorieas = { "day1":380,"day2": 420,"day3":650}
+# myvar = pd.Series(Colorieas)
+# print(myvar)
+
+# DataFrames
+
+# Data = {"Name":["Sayem","Rakib","Sifat"],"Age":[23,23,25]}
+# df = pd.DataFrame(Data)
+# print(df)
+
+# Data = {
+#     "Colories" : [320,230,409],
+#     "Duration": [2,3,4]
+# }
+# myvar = pd.DataFrame(Data)
+# print(myvar)
+
+
